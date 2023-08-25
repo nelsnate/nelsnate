0333f8684ef400b2ceafe6a7b8139a76860adc05
nelsnate <122180150+nelsnate@users.noreply.github.com>
2023-08-26 00:47:40 +0300

    Add files via upload

---
+++ nelsnate/data/config.js
@@ -0,0 +1,51 @@
+export const meta = {
+  title: 'Nelson Nathan - Web Developer and Designer',
+  description: 'Hey, I\'m Nelson. I\'m a web developer and designer building things people love.'
+}
+
+export const hero = {
+  title: "Hey there! I'm Nelson Nathan.",
+  desc: "I'm a graphic designer and web developer building things people love."
+};
+
+export const projects = {
+  title: 'My Links',
+  desc: "Here are links of my portfolio and socials.",
+  projects: [
+    {
+      title: 'Behance',
+      description: 'Nelson Nathan-Portfolio',
+      link: 'https://behance.com/nelsnate',
+      github: ''
+    },
+    {
+      title: 'GitHub',
+      description: 'Coding stuff!',
+      github: 'github.com/nelsnate',
+      link: ''
+    },
+    {
+      title: 'Movie Collection',
+      description: 'Movies & TV!',
+      link: 'https://mustapp.com/nelsnate/watched',
+      github: ''
+    }
+  ]
+};
+
+export const stack = {
+  title: 'My Stack',
+  stack: [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'Illustrator',
+    'Photoshop'
+  ]
+};
+
+export const contact = {
+  title: 'Contact Me',
+  email: 'nelznate@gmail.com',
+  link: 'nelznate',
+};
